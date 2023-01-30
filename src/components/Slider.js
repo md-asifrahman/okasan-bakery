a4465bd7560934c5498fc4be90fa31ccd127a915
Md. Asif Rahman <118436733+md-asifrahman@users.noreply.github.com>
2023-01-30 11:40:58 +0600

    Update Slider.js

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -10,7 +10,7 @@ export default function Slider() {
     >
       <div className="carousel-inner">
         <div className="carousel-item active ">
-          <img className="d-block w-100" src="images/home.jpg" alt="First slide" />
+          <img className="d-block w-100" src="https://bleudoorbakery.com/wp-content/uploads/2020/12/Bleu-Door-Bakery-pastries.jpg" alt="First slide" />
           <div className="carousel-caption mx-auto ">
             <h5>When you eat food with your family and friends, it always tastes better and everything tastes good when you're hungry.</h5>
             <button type="button" className="btn btn-success">Order Now</button>
